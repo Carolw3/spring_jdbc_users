f36aaac2efc346ba073563c8e1e18a855c33fe02
Carolw3 <carolina.melguizos3@gmail.com>
2025-11-05 18:45:11 +0100

    primer commit de la branca fitxers

---
+++ com-ra2-user/src/main/java/com/ra2/user/com_ra2_user/service/UserService.java
@@ -0,0 +1,15 @@
+package com.ra2.user.com_ra2_user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ra2.user.com_ra2_user.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    Publi
+}
